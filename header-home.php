21518d393d17b271ba34017ae562bae8698b054e
khenryjoe <joseph.ketterer@gmail.com>
2019-10-13 17:15:34 +0100

    first commit

---
+++ header-home.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package joes_theme
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div class ='nav-container'>
+<nav class="navigation">
+  <?php wp_nav_menu( array(
+		'theme_location' => 'menu-1',
+        'menu_id'        => 'primary-menu',
+        'menu_class' => 'navigation-menu'
+        // 'container_class' => 'navigation-main'
+			  ));
+			?>
+</nav>
+			</div>
+
+
---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * algos Theme Customizer
+ *
+ * @package algos
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function algos_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'algos_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'algos_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'algos_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function algos_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function algos_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function algos_customize_preview_js() {
+	wp_enqueue_script( 'algos-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'algos_customize_preview_js' );
