973d9aaa4d89fdf19042f56b3c090bfbcc365925
khenryjoe <joseph.ketterer@gmail.com>
2019-10-19 15:30:14 +0100

    style

--- front-page.php
+++ front-page.php
@@ -1,38 +1,13 @@
+<?php 
+$intro = get_field('introtext')
+?>
 <?php get_header('home');?>
    <main class="main-body">
        <div class="rounded"><div class="text"><h2>Data structures and algorithms in Javascript and Python</h2></div> </div>
-	<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-        ?>
-        </main>
+       <div class="container-intro"><p class="intro"><?php echo $intro; ?></p></div>
+       <div class="container-articles"><article class="home-article"><div class="image1"></div></article><article class="home-article">
+       <div class="image2"></div>
+       </article></div>
+    </main>
 
 <?php get_footer();?>
