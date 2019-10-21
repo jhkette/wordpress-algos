1286d5b9a514df31955879866deaae5e05b1066d
khenryjoe <joseph.ketterer@gmail.com>
2019-10-21 15:00:24 +0100

    scss

--- front-page.php
+++ front-page.php
@@ -5,14 +5,26 @@ $intro = get_field('introtext')
    <main class="main-body">
        <div class="rounded"><div class="text"><h2>Data structures and algorithms in Javascript and Python</h2></div> </div>
        <div class="container-intro"><p class="intro"><?php echo $intro; ?></p></div>
-       <div class="container-articles"><article class="home-article"><div class="image1"></div></article><article class="home-article">
+       <div class="container-articles"><article class="home-article"><div class="image1"></div>
+       <p>observed that, if the sperm whale, once struck, blind rage,</p>
+       <p>Again, it is very often observed that, if the sperm whale, once struck, blind rage, as with wilful, 
+       deliberate designs of destruction to his pursuers; nor is</p>
+        </article>
+        <article class="home-article">
        <div class="image2"></div>
+       <p>observed that, if the sperm whale, once struck, blind rage,</p>
+       <p>Again, it is very often observed that, if the sperm whale, once struck, blind rage, as with wilful, 
+       deliberate designs of destruction to his pursuers; nor is</p>
        </article></div>
 
 
-       <div class="container-articles"><article class="home-article"><div class="image3"></div></article><article class="home-article">
-       
-       </article></div>
+       <div class="container-articles"><article class="home-article"><div class="image3"></div></article>
+       <article class="home-article">
+       <p>observed that, if the sperm whale, once struck, blind rage,</p>
+       <p>Again, it is very often observed that, if the sperm whale, once struck, blind rage, as with wilful, 
+       deliberate designs of destruction to his pursuers; nor is it without conveying some eloquent indication of his character</p>
+       </article>
+    </div>
     </main>
 
 <?php get_footer();?>
